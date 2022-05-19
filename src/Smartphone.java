ebcf02f2f410839147c0a3df1feb1716acd91374
Reinhold Buchinger <buc@htl.rennweg.at>
2022-05-19 14:44:49 +0200

    add first code

---
+++ src/Smartphone.java
@@ -0,0 +1,14 @@
+public class Smartphone {
+    String osSystem;     //Betriebssystem
+    int gewicht = 172;   //Gewicht in g
+
+
+     void klingle() {
+        System.out.println("Beep Beep Beep Beep");
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
